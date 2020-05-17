7a99f6e71ee14015fd16476cb3a67cbe6638f914
jvog <62656589+jensvog@users.noreply.github.com>
2020-05-17 09:57:08 +0200

    Remove Todo text

--- src/lambda/auth/auth0Authorizer.ts
+++ src/lambda/auth/auth0Authorizer.ts
@@ -62,9 +62,6 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODODONE: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
   const cert = await Axios.get(jwksUrl);
   logger.info('Token', token);
   logger.info('Cert', cert.data);
